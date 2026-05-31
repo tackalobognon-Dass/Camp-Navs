1f6480c70abe93d7e7e816ef978a8350c9920832
tackalobognon-Dass <tackalobognon@gmail.com>
2026-05-31 22:40:03 +0000

    Fix App.jsx retrait EnfantsPage SoireePage inexistants

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,11 @@ import LoginPage from './pages/admin/LoginPage'
 import DashboardPage from './pages/admin/DashboardPage'
 import MembresPage from './pages/admin/MembresPage'
 import CampeursPage from './pages/admin/CampeursPage'
-import EnfantsPage from './pages/admin/EnfantsPage'
 import TresoreriePage from './pages/admin/TresoreriePage'
 import AnnoncesAdminPage from './pages/admin/AnnoncesAdminPage'
 import ChantsAdminPage from './pages/admin/ChantsAdminPage'
 import DocumentsAdminPage from './pages/admin/DocumentsAdminPage'
 import ProgrammeAdminPage from './pages/admin/ProgrammeAdminPage'
-import SoireePage from './pages/admin/SoireePage'
 import SantePage from './pages/admin/SantePage'
 import LogistiquePage from './pages/admin/LogistiquePage'
 import SuiviPostCampPage from './pages/admin/SuiviPostCampPage'
@@ -34,7 +32,6 @@ import ParametresPage from './pages/admin/ParametresPage'
 
 function App() {
   const [splashDone, setSplashDone] = useState(false)
-
   return (
     <>
       {!splashDone && <SplashScreen onFinish={() => setSplashDone(true)} />}
@@ -55,13 +52,11 @@ function App() {
         <Route path="/admin" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
         <Route path="/admin/membres" element={<PrivateRoute><MembresPage /></PrivateRoute>} />
         <Route path="/admin/campeurs" element={<PrivateRoute><CampeursPage /></PrivateRoute>} />
-        <Route path="/admin/enfants" element={<PrivateRoute><EnfantsPage /></PrivateRoute>} />
         <Route path="/admin/tresorerie" element={<PrivateRoute><TresoreriePage /></PrivateRoute>} />
         <Route path="/admin/annonces" element={<PrivateRoute><AnnoncesAdminPage /></PrivateRoute>} />
         <Route path="/admin/chants" element={<PrivateRoute><ChantsAdminPage /></PrivateRoute>} />
         <Route path="/admin/documents" element={<PrivateRoute><DocumentsAdminPage /></PrivateRoute>} />
         <Route path="/admin/programme" element={<PrivateRoute><ProgrammeAdminPage /></PrivateRoute>} />
-        <Route path="/admin/soiree" element={<PrivateRoute><SoireePage /></PrivateRoute>} />
         <Route path="/admin/sante" element={<PrivateRoute><SantePage /></PrivateRoute>} />
         <Route path="/admin/logistique" element={<PrivateRoute><LogistiquePage /></PrivateRoute>} />
         <Route path="/admin/suivi-post-camp" element={<PrivateRoute><SuiviPostCampPage /></PrivateRoute>} />
